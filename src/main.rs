0e77c4076d260e2d6f5686270c9dad55366b6620
NizTheDev <arehman08@hotmail.com>
2022-10-03 23:02:48 +0500

    added /event route which returns an inifinite stream of server sent events

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
 #[macro_use]
 extern crate rocket;
 
-use rocket::{
-    serde::{Deserialize, Serialize},
-    tokio::sync::broadcast::channel,
-    tokio::sync::broadcast::Sender,
-    State,
-};
-
 use rocket::form::Form;
+use rocket::tokio::select;
+use rocket::{
+    response::stream::{Event, EventStream},
+    serde::{Deserialize, Serialize},
+    tokio::sync::broadcast::{channel, error::RecvError, Sender},
+    Shutdown, State,
+};
 
 #[derive(Debug, Clone, FromForm, Serialize, Deserialize)]
 #[serde(crate = "rocket::serde")]
@@ -26,11 +26,38 @@ fn test() -> &'static str {
     "Meow World!"
 }
 
+/*
+  Receive a message from a form submission and broadcast it to any receivers.
+*/
 #[post("/message", data = "<form>")]
 fn post(form: Form<Message>, queue: &State<Sender<Message>>) {
     // Sending will fail if there are no active listeners
 
     let _res = queue.send(form.into_inner());
+}
+
+/*
+  Returns an infinite stream of server-sent events. Each event is a message
+  pulled from a broadcast queue sent by the `post` handler.
+*/
+#[get("/events")]
+async fn events(queue: &State<Sender<Message>>, mut end: Shutdown) -> EventStream![] {
+    let mut rx = queue.subscribe();
+
+    EventStream! {
+      loop {
+          let msg = select! {
+            msg = rx.recv() => match msg {
+              Ok(msg) => msg,
+              Err(RecvError::Closed) => break,
+              Err(RecvError::Lagged(_)) => continue,
+            },
+            _ = &mut end => break,
+          };
+
+          yield Event::json(&msg)
+      }
+    }
 }
 
 #[launch]
